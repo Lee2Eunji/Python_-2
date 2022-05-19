4e22b83f3ff7db9127d9bcf1191d34a47d123987
eunji <eunjijjang11@naver.com>
2022-05-19 23:10:54 +0900

    Homework

--- List_assignment.py
+++ List_assignment.py
@@ -2,17 +2,20 @@ List = []
 for x in range(1, 16):
     num = int(x)
     List.append(num)
-print(List)
-if List == 14:
     print(List)
-if x * 2:
-    del(List[x])
+if range == 14:
     print(List)
-if List * 3:
-    del(List[x])
-    print(List)
+
+i = 1
+while i <= 7:
+    print(i * 2)
+    i = i + 1
+    if i == i * 2:
+        del(List[i])
+if i == i * 3:
+    del(List[i])
 
 print(List.pop(1))
 List.insert(1, 2)
-List.extend(3)
+List.extend("3")
 List.sort()
